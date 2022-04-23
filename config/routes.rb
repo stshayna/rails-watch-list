904bd6b7c591c3b047d020a11fac4386e5b6bc9f
stshayna@gmail.com <stshayna@gmail.com>
2022-04-23 15:47:29 -0400

    cl_image_tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # auto-add "/lists" at the root path
   root to: redirect('/lists')
+
+  # root path remains the same, but links root to lists#index action
   # root to: 'lists#index'
 
   resources :lists, except: %i[edit update destroy] do
