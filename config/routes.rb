270fdf2c0b97bfadf1b6243b7efa3fee4b273207
stshayna@gmail.com <stshayna@gmail.com>
2022-04-23 15:01:40 -0400

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: redirect('/lists')
+  # root to: 'lists#index'
+
   resources :lists, except: %i[edit update destroy] do
     resources :bookmarks, only: %i[new create]
   end
