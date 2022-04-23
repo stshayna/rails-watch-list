a7d0be27b7614838a3e6e09c14f4ac2f6a23e65b
stshayna@gmail.com <stshayna@gmail.com>
2022-04-23 11:50:48 -0400

    add platform x86_64-linux

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,13 @@ require 'json'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Movie.destroy_all
+
 url = URI.open('https://tmdb.lewagon.com/movie/top_rated').read
 doc = JSON.parse(url)
 
-doc['results'][0..5].each do |hash|
-  Movie.create(
+doc['results'][0..11].each do |hash|
+  Movie.create!(
     title: hash['original_title'],
     overview: hash['overview'],
     poster_url: "https://image.tmdb.org/t/p/original/#{hash['poster_path']}",
